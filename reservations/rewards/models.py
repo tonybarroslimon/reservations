fd49569e3677df01e17cd5f12f4e3b9beecde9d5
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-25 12:37:31 -0800

    updated the models.py file

--- reservations/rewards/models.py
+++ reservations/rewards/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from make.models import Make
+
+class Rewards(models.Model):
+    reward_number = models.ForeignKey(Make.phone_number)
+    reward_email = models.ForeignKey(Make.email)
+    reward_points = models.IntegerField(default = 0)
+    receipt_number = models.IntegerField(max_length = 30)
