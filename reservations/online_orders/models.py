f80bdcd2c31ddc4262fe1fee7b251e61b4183ab5
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-25 13:22:55 -0800

    created the beginnings of the models.py file

--- reservations/online_orders/models.py
+++ reservations/online_orders/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 from menu.models import Menu
+from contacts.models import Group
+
+class Order(models.Model):
+    name = models.ForeignKey(Group.member_name)
+    cart = models.ForeignKey(Menu.name)
+    total = models.ForeignKey(Menu.price)
+    special_instructions = models.ForeignKey(Menu.special_instructions)
+    
+    def __unicode__(self):
+        return self.name
+    
 
 
-
-# Create your models here.
