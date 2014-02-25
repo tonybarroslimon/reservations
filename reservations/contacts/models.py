575138cf56962df80d933eb50d9b5923c60d1487
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-25 13:22:06 -0800

    fixed model syntax

--- reservations/contacts/models.py
+++ reservations/contacts/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 from make.models import Make
 
-class Group(models.Model)
+class Group(models.Model):
     group_name = models.CharField(max_length = 50)
-    member_name = models.ForeignKey(Make.first_name + " " Make.last_name)
+    member_name = models.ForeignKey(Make.first_name + " " + Make.last_name)
     member_phone_number = models.ForeignKey(Make.phone_number)
     member_email = models.ForeignKey(Make.email)
     birthday = models.DateField()
