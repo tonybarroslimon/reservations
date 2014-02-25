f5e4ac83e3f84066844cb7e6fd65ac62f8e7f086
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-25 09:31:39 -0800

    removed secret key

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j^os4016(a*2xe&tcgdh#!+e45^6(+#%%#$125)8wq-@ri%lvv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
