15977c2d7eb3a27ff27e4f9924eedc6ac507ac55
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-25 11:18:49 -0800

    created the models.py file

--- reservations/contacts/models.py
+++ reservations/contacts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from make.models import Make
+
+class Group(models.Model)
+    group_name = models.CharField(max_length = 50)
+    member_name = models.ForeignKey(Make.first_name + " " Make.last_name)
+    member_phone_number = models.ForeignKey(Make.phone_number)
+    member_email = models.ForeignKey(Make.email)
+    birthday = models.DateField()
+    
+    def __unicode__(self):
+        return self.group_name
