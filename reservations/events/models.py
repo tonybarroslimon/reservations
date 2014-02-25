ceb4526f1b2e5b412e380da93e6d0be16159418d
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-25 10:53:55 -0800

    fixed a few things and made the models better

--- reservations/events/models.py
+++ reservations/events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Calendar(models.Model):
+    event_name = models.CharField(max_length = 150)
+    special_notes = models.CharField(max_length = 150)
+    event_date = models.DateTimeField('Event Date')
+    
+    def __unicode__(self):
+        return self.event_name
+    
--- reservations/make/models.py
+++ reservations/make/models.py
@@ -4,21 +4,23 @@ class Make(models.Model):
     first_name = models.Charfield(max_length = 150)
     last_name = models.Charfield(max_length = 150)
     special_requests = models.Charfield(max_length = 500)
-    date = models.DateAndTimeField(auto_now_update)
-    email = models.EmailField()
+    reservation_date = models.DateTimeField()
+    reservation_made = models.DateTimeField(default = datetime.datetime.now)
+    email = models.CharField(max_length = 300)
     phone_number = models.IntField(max_length = 10)
     
     def __unicode__(self):
-        return self.phone_number
+        return self.last_name
     
     
 class Details(models.Model):
-    name = models.Charfield(max_length = 150)
-    available_spots = models.Intfield(max_length = 100)
-    time_slots = models.Intfield(max_length = 100)
+    restaurant_name = models.CharField(max_length = 150)
+    details = models.ForeignKey(Make)
+    available_spots = models.TimeField()
+    time_slots = models.IntField(max_length = 100)
     
     def __unicode__(self):
-        return self.name
+        return self.restaurant_name
     
     
 
