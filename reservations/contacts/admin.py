98c386bda868b8cddc1554d7c92d65b79c7daf0e
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-26 13:05:19 -0800

    updated all of the admin.py files

--- reservations/contacts/admin.py
+++ reservations/contacts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+import models
+
+admin.site.register(models.Group)
+
 # Register your models here.
--- reservations/make/admin.py
+++ reservations/make/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+import models
+
+admin.site.register(models.Make)
+
+admin.site.register(models.Details)
