d68a7b9eea6b7f2ee24c13896c7d9fb1aa68eab0
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-27 13:01:36 -0800

    created the meta class within the Order class

--- reservations/online_orders/models.py
+++ reservations/online_orders/models.py
@@ -4,14 +4,15 @@ from menu.models import Menu
 from contacts.models import Group
 
 class Order(models.Model):
-    first_name = models.ForeignKey(Group.first_name)
-    last_name = models.ForeignKey(Group.last_name)
-    cart = models.ForeignKey(Menu.name)
-    total = models.ForeignKey(Menu.price)
-    special_instructions = models.ForeignKey(Menu.special_instructions)
+    class Meta:
+        first_name = Group.first_name
+        last_name = Group.last_name
+        cart = Menu.name
+        total = Menu.price
+        special_instructions = Menu.special_instructions
     
     def __unicode__(self):
-        return self.name
+        return self.last_name
     
 
 
