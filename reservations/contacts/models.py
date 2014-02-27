a3e4b1f286a2a1282b4f8b21179af9f4d08cb398
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-27 09:49:19 -0800

    I think this will work with the models.py file

--- reservations/contacts/models.py
+++ reservations/contacts/models.py
@@ -6,7 +6,6 @@ class Group(models.Model):
     group_name = models.CharField(max_length = 50)
     first_name = models.CharField(max_length = 150)
     last_name = models.CharField(max_length = 150)
-    full_name = first_name + " " + last_name
     member_phone_number = models.ForeignKey(Make.phone_number)
     member_email = models.ForeignKey(Make.email)
     birthday = models.DateField()
