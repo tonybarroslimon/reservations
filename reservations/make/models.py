3c649242d6a6887dea5467d27fe3f4d16d6f3152
tonybarroslimon <tonybarroslimon@outlook.com>
2014-02-27 09:36:26 -0800

    fixed more models.py bugs

--- reservations/make/models.py
+++ reservations/make/models.py
@@ -7,7 +7,7 @@ class Make(models.Model):
     reservation_date = models.DateTimeField()
     reservation_made = models.DateTimeField(auto_now_add = True)
     email = models.CharField(max_length = 300)
-    phone_number = models.IntField(max_length = 10)
+    phone_number = models.IntegerField(max_length = 10)
     
     def __unicode__(self):
         return self.last_name
@@ -17,7 +17,7 @@ class Details(models.Model):
     restaurant_name = models.CharField(max_length = 150)
     details = models.ForeignKey(Make)
     available_spots = models.TimeField()
-    time_slots = models.IntField(max_length = 100)
+    time_slots = models.IntegerField(max_length = 100)
     
     def __unicode__(self):
         return self.restaurant_name
